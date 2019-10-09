c758d053e1d6e7632cd8c033e20deccac68197dd
Ahmed Elnably <aelnably@microsoft.com>
2019-10-09 11:29:03 -0700

    Update __init__.py

--- httpTrigger/__init__.py
+++ httpTrigger/__init__.py
@@ -19,6 +19,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello {name}!")
     else:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
+             "Please pass a name in the query string or in the request body",
              status_code=400
         )
