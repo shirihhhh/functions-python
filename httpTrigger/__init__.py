f776b8eeeb473637647a675a7a530ecb4a58b0d6
shirihhhh <60128832+shirihhhh@users.noreply.github.com>
2020-02-17 19:51:06 +0200

    Update __init__.py

--- httpTrigger/__init__.py
+++ httpTrigger/__init__.py
@@ -16,7 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello awesome {name} meetup!!!!")
+        return func.HttpResponse(f"Hello great {name} meetup!!!!")
     else:
         return func.HttpResponse(
              "Please pass a name in the query string or in the request body",
